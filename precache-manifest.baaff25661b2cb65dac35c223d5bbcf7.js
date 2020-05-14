95e3657aad564da53ea6574ce730d8b84d6112e5
Tabitori <tabitori@outlook.com>
2020-05-14 19:06:58 +0700

    Initial implementation

---
+++ precache-manifest.baaff25661b2cb65dac35c223d5bbcf7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44e7ea386ea44087e5c1347e555d707b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a4c19972c29d2c5653d0",
+    "url": "./static/js/2.198dda91.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.198dda91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8749e4fddab760e34bd",
+    "url": "./static/js/main.31696889.chunk.js"
+  },
+  {
+    "revision": "dbdbd1cc6f6507e79655",
+    "url": "./static/js/runtime-main.59b90122.js"
+  },
+  {
+    "revision": "ce76349de047b1bf12a9829aef027c2b",
+    "url": "./static/media/icon_item_card_legend.ce76349d.png"
+  },
+  {
+    "revision": "7e39df87544d63074653477e4103715c",
+    "url": "./static/media/icon_item_card_stat.7e39df87.png"
+  },
+  {
+    "revision": "f9c2452aa3eeacac24ad4c6ac3dcac1d",
+    "url": "./static/media/icon_item_legend_card_fragment_lv2.f9c2452a.png"
+  }
+]);
